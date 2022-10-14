873bb8f806403c7d5b8539cb822b6284b8f6dbc1
machao-hyron <ma_chao@hyron.com>
2022-10-14 14:07:45 +0800

    init

---
+++ study/src/main/java/com/example/demo/service/impl/LoginServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import com.example.demo.dto.LoginDto;
+import com.example.demo.entity.TUserInfo;
+import com.example.demo.entity.TUserInfoExample;
+import com.example.demo.mapper.TUserInfoMapper;
+import com.example.demo.service.LoginService;
+
+@Service
+public class LoginServiceImpl implements LoginService {
+	
+	@Autowired
+	private TUserInfoMapper userInfoMapper;
+
+	@Override
+	public boolean loginAcntCheck(LoginDto dto) {
+		
+		TUserInfoExample example = new TUserInfoExample();
+		example.createCriteria().andUserIdEqualTo(Integer.parseInt(dto.getId())).andPwdEqualTo(dto.getPwd());
+		List<TUserInfo> userInfoList = userInfoMapper.selectByExample(example);
+		if(!CollectionUtils.isEmpty(userInfoList)) {
+			return true;
+		}
+		return false;
+	}
+
+}
