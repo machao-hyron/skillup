79ec26d217e18f301c4cce89ab52a16d54a39ba3
jcy15189590021 <1692255675@qq.com>
2022-10-16 18:39:32 +0800

    登录拦截器添加

---
+++ study/src/main/java/com/example/demo/Interceptor/LoginInterceptor.java
@@ -0,0 +1,42 @@
+package com.example.demo.Interceptor;
+
+import com.example.demo.constant.StringConstant;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 登录拦截器
+ */
+@Slf4j
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        log.info("preHandle拦截的请求路径是{}",requestURI);
+        //登录检查逻辑
+        HttpSession session = request.getSession();
+        if(session.getAttribute(StringConstant.SESSION_USERINFO) != null){
+            //放行
+            return true;
+        }
+        //拦截住。未登录。跳转到登录页
+        request.getRequestDispatcher("/login").forward(request,response);
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("postHandle执行");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.info("afterCompletion执行");
+    }
+}
---
+++ study/src/main/java/com/example/demo/config/AdminInterceptorConfig.java
@@ -0,0 +1,17 @@
+package com.example.demo.config;
+
+import com.example.demo.Interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AdminInterceptorConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/**")  //所有请求都被拦截包括静态资源
+                .excludePathPatterns("/login","/css/**","/favicon.ico","/assets/**",
+                        "/js/**"); //放行的请求
+    }
+}
