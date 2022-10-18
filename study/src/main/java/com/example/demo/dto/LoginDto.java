07200fbc4a33a093566547e159a50f5ff77132c6
helloearths <1907636009@qq.com>
2022-10-18 23:09:14 +0800

    study now

--- study/src/main/java/com/example/demo/dto/LoginDto.java
+++ study/src/main/java/com/example/demo/dto/LoginDto.java
@@ -19,5 +19,23 @@ public class LoginDto implements Serializable{
 	
 	/** PWD */
 	private String pwd;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+	
 	
 }
