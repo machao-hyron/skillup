8e6968114fda408c32c6d2375e3a8962a5e47cbb
lu <834808263@qq.com>
2022-11-11 22:12:24 +0800

    luguanyu-java1

--- ats/dbSQL/atsdb.sql
+++ ats/dbSQL/atsdb.sql
@@ -10,7 +10,7 @@ USE `ats_demo`;
 */
 DROP TABLE IF EXISTS `t_number`;
 CREATE TABLE IF NOT EXISTS `t_number` (
-  `job_no` VARCHAR(10) NOT NULL ,
+  `job_no` VARCHAR(32) NOT NULL ,
   `mail` VARCHAR(50) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
   `login_datetime` TIMESTAMP NULL DEFAULT NULL,
@@ -25,12 +25,14 @@ CREATE TABLE IF NOT EXISTS `t_number` (
 */
 DROP TABLE IF EXISTS `t_emp`;
 CREATE TABLE IF NOT EXISTS `t_emp` (
-  `job_no` VARCHAR(10) NOT NULL ,
-  `dep_id` VARCHAR(10) NOT NULL,
+  `job_no` VARCHAR(32) NOT NULL ,
+  `dep_id` VARCHAR(32) NOT NULL,
   `project_id` VARCHAR(32) NOT NULL,
   `emp_name` VARCHAR(32) NOT NULL,
   `sex` CHAR(1) NOT NULL,
   `birthday_datetime` TIMESTAMP NULL DEFAULT NULL,
+  `leave_school_time` DATE NULL DEFAULT NULL,
+  `school` VARCHAR(32) NOT NULL,
   `join_time` TIMESTAMP NULL DEFAULT NULL,
   `on_job_flag` CHAR(1) NOT NULL,
   `leave_time` TIMESTAMP NULL DEFAULT NULL,
@@ -41,4 +43,23 @@ CREATE TABLE IF NOT EXISTS `t_emp` (
   `update_user` VARCHAR(32) NOT NULL ,
   PRIMARY KEY (`job_no`),
   INDEX(`emp_name`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+/*
+工作履历表创建
+*/
+DROP TABLE IF EXISTS `t_record`;
+CREATE TABLE IF NOT EXISTS `t_record` (
+  `no` INT(32) NOT NULL AUTO_INCREMENT,
+  `job_no`VARCHAR(32) NOT NULL,
+  `from_time` DATE NULL DEFAULT NULL,
+  `to_time`   DATE NULL DEFAULT NULL,
+  `company_name` VARCHAR(32) NOT NULL ,
+  `position` VARCHAR(32) NOT NULL,
+  `login_datetime` TIMESTAMP NULL DEFAULT NULL,
+  `login_user` VARCHAR(32) NOT NULL ,
+  `update_datetime` TIMESTAMP NULL DEFAULT NULL,
+  `update_user` VARCHAR(32) NOT NULL ,
+  PRIMARY KEY (`no`),
+  FOREIGN KEY(`job_no`) REFERENCES t_emp(`job_no`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
