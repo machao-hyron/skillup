db8915763e4820cf95821cc7a7588cadbe60815a
lu <834808263@qq.com>
2022-11-20 11:47:00 +0800

    +

--- ats/src/main/java/com/hyron/atsdemo/controller/EmpController.java
+++ ats/src/main/java/com/hyron/atsdemo/controller/EmpController.java
@@ -29,8 +29,8 @@ import java.util.List;
 public class EmpController {
     @Autowired
     private EmpService empService;
-    @Value("${upload.dir}")
-    private String realPath;
+//    @Value("${upload.dir}")
+//    private String realPath;
 
     @RequestMapping(value = "/findAll",method = RequestMethod.GET)
     public String findAll(HttpServletRequest request){
@@ -43,30 +43,31 @@ public class EmpController {
     @RequestMapping("/import")
     public String importExcel(MultipartFile excelFile) throws Exception{
       log.info("文件名：[{}]",excelFile.getOriginalFilename());
-      //进行excel导入
+        //进行excel导入
         ImportParams params = new ImportParams();
         //params.setTitleRows(1);//标题的位置
         params.setHeadRows(1);//header列的位置
         params.setStartSheetIndex(4);//读取的sheet为第5个
         //params.setImportFields(new String[]{"工号","部门"});
-        List<Emp> emps = ExcelImportUtil.importExcel(excelFile.getInputStream(), Emp.class, params);
+        List<Emp> emps = ExcelImportUtil.importExcel(excelFile.getInputStream(), Emp.class, params);//实现文件导入
        // emps.forEach(System.out::println);
-        empService.setDataFromExcel(emps);
+        empService.setDataFromExcel(emps);//存入数据库
         return "redirect:findAll";//上传完成后跳转到查询的路径
     }
     @RequestMapping("/export")
     public String exportExcel(HttpServletResponse response) throws Exception{
         List<Emp> emps = empService.findAll();
-        emps.forEach(emp -> {
-            emp.setEmpImg(realPath+"/"+emp.getEmpImg());
-        });
+//        emps.forEach(emp -> {
+//            emp.setEmpImg(realPath+"/"+emp.getEmpImg());
+//        });
         log.info("导出Excel");
         Workbook workbook = ExcelExportUtil.exportExcel(new ExportParams("员工信息一揽", "员工信息"), Emp.class, emps);
-        response.setHeader("content-disposition","attachment;fileName"+ URLEncoder.encode("用户列表.xls","UTF-8"));
-        ServletOutputStream outputStream = response.getOutputStream();
-        workbook.write(outputStream);
+        response.setHeader("content-disposition","attachment;fileName"+ URLEncoder.encode("用户列表.xls","UTF-8"));//对响应头转码，防止中午乱码
+        ServletOutputStream outputStream = response.getOutputStream();//输出流
+        workbook.write(outputStream);//将流写入
+        //关闭流
         outputStream.close();
         workbook.close();
-        return "redirect:findAll";
+        return "redirect:findAll";//上传完成后跳转到查询的路径
     }
 }
