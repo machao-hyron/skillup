74af4347bc4473ded56e6da00b5cd74c74deb341
chrislhw <2422300538@qq.com>
2022-11-20 22:21:12 +0800

    DB设计书及DDL

---
+++ 01_设计书/02_DB定义书/DDL/DDL.sql
@@ -0,0 +1,76 @@
+-- 账号情报表
+CREATE TABLE IF NOT EXISTS `t_account_info` (
+  `employee_id` char(7) NOT NULL,
+  `e_mail` VARCHAR(50) NOT NULL,
+  `psw` varchar(20) NOT NULL,
+  `create_time` Datetime NOT NULL,
+  `create_user_id` char(7) NOT NULL,
+  `update_time` Datetime NULL DEFAULT NULL,
+  `update_user_id` char(7) NULL DEFAULT NULL,
+  PRIMARY KEY (`employee_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- 员工情报表
+CREATE TABLE IF NOT EXISTS `t_employee_info` (
+  `employee_id` char(7) NOT NULL,
+  `department_id` char(4) NOT NULL,
+  `project_id` varchar(5) NOT NULL,
+  `employee_name` varchar(10) NOT NULL,
+  `employee_sex` char(1) NOT NULL,
+  `employee_birth` Date NOT NULL,
+  `graduation_year` YEAR NOT NULL,
+  `graduation_university` varchar(50) NOT NULL,
+  `induction_date` Date NOT NULL,
+  `on_job_flg` tinyint(1) NOT NULL,
+  `resignation_date`Date NOT NULL,
+  `employee_img` varchar(50) NOT NULL,
+  `create_time` Datetime NOT NULL,
+  `create_user_id` char(7) NOT NULL,
+  `update_time` Datetime NULL DEFAULT NULL,
+  `update_user_id` char(7) NULL DEFAULT NULL,
+  PRIMARY KEY (`employee_id`,`department_id`,`project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- 工作履历表
+CREATE TABLE IF NOT EXISTS `t_work_experience` (
+  `employee_id` char(7) NOT NULL,
+  `from_year` YEAR NOT NULL,
+  `to_year` YEAR NOT NULL,
+  `position` varchar(10) NOT NULL,
+  `create_time` Datetime NOT NULL,
+  `create_user_id` char(7) NOT NULL,
+  `update_time` Datetime NULL DEFAULT NULL,
+  `update_user_id` char(7) NULL DEFAULT NULL,
+  PRIMARY KEY (`employee_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+-- 技能信息表
+CREATE TABLE IF NOT EXISTS `t_skill_info` (
+  `employee_id` char(7) NOT NULL,
+  `java` tinyint(1) NOT NULL,
+  `c_sharp` tinyint(1) NOT NULL,
+  `android` tinyint(1) NOT NULL,
+  `ios` tinyint(1) NOT NULL,
+  `c_plus` tinyint(1) NOT NULL,
+  `net` tinyint(1) NOT NULL,
+  `spring` tinyint(1) NOT NULL,
+  `oracle` tinyint(1) NOT NULL,
+  `mysql` tinyint(1) NOT NULL,
+  `sqlserver` tinyint(1) NOT NULL,
+  `jsp` tinyint(1) NOT NULL,
+  `jquery` tinyint(1) NOT NULL,
+  `css` tinyint(1) NOT NULL,
+  `html` tinyint(1) NOT NULL,
+  `thymeleaf` tinyint(1) NOT NULL,
+  `angular` tinyint(1) NOT NULL,
+  `vue` tinyint(1) NOT NULL,
+  `react` tinyint(1) NOT NULL,
+  `create_time` Datetime NOT NULL,
+  `create_user_id` char(7) NOT NULL,
+  `update_time` Datetime NULL DEFAULT NULL,
+  `update_user_id` char(7) NULL DEFAULT NULL,
+  PRIMARY KEY (`employee_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
