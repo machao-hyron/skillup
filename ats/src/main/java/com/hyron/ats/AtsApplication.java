c001bc0893c512de46577255567ba92c75f38a28
lu <834808263@qq.com>
2022-11-27 10:57:52 +0800

    +

--- ats/src/main/java/com/hyron/ats/AtsApplication.java
+++ ats/src/main/java/com/hyron/ats/AtsApplication.java
@@ -2,12 +2,13 @@ package com.hyron.ats;
 
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.hyron.ats.mapper")
 public class AtsApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(AtsApplication.class, args);
     }
 }
